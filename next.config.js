a36ed21f3264c66adcdfb8e76dac11c10a8562d4
Tosh Moltbot <ToshMoltBot@gmail.com>
2026-02-16 01:06:54 -0500

    fix: add basePath for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: '/saas-graveyard',
+  assetPrefix: '/saas-graveyard/',
 }
 
 module.exports = nextConfig

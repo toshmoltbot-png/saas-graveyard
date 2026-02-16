c276bb4ca8729f7359e7e8d7ff409d10bb381a8e
Tosh Moltbot <ToshMoltBot@gmail.com>
2026-02-16 01:06:49 -0500

    Updates

--- _next/static/gNv-wL9fM1BciEf4tRCF5/_buildManifest.js
+++ _next/static/gNv-wL9fM1BciEf4tRCF5/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/saas-graveyard//_next/:path+",
+        "destination": "/saas-graveyard/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
